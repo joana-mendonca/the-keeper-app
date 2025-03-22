922bd123a53261e7e8dd54d9f61799af0638aa39
joana-mendonca <joanaqmendonca@hotmail.com>
2025-03-22 17:46:21 +0000

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import EventNoteIcon from '@mui/icons-material/EventNote';
+
+function Header() {
+  return (
+    <header>
+      <EventNoteIcon />
+      <h1>   
+        Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
